470f8842738769ed6b044c9a6d0b1b6c45b0123e
joannajzou <joannajzou@gmail.com>
2025-01-30 11:01:08 -0500

    correct indexing on Ito integration

--- src/Samplers/ula.jl
+++ src/Samplers/ula.jl
@@ -1,12 +1,13 @@
 struct ULA <: Sampler
     ϵ :: Real   # step size
     β :: Real   # inverse temperature
+    seed :: Union{Integer, Nothing}
 end
-
+ULA(ϵ::Real, β::Real) = ULA(ϵ, β, nothing)
 
 # performs a single Euler-Maruyama step
 function propose(x0::Real, gradlπ::Function, ula::ULA)
-    ξt = rand(Normal(0,sqrt(ula.ϵ))) 
+    ξt = sqrt(ula.ϵ) * randn()
     xt = x0 - gradlπ(x0) * ula.ϵ + sqrt(2/ula.β) * ξt
     return xt, ξt
 end
@@ -22,26 +23,35 @@ end
 
 # samples using ULA
 function sample(gradlπ::Function, sampler::ULA, T::Int64, x0::Real)
-    xsim = zeros(T); xsim[1] = x0
+    xsim = zeros(T+1); xsim[1] = x0
     ξsim = zeros(T)
 
-    for t = 2:T
-        # simulate
-        xsim[t], ξsim[t] = propose(xsim[t-1], gradlπ, sampler)
+    if sampler.seed != nothing
+        Random.seed!(sampler.seed)
     end
-    return xsim, ξsim
+
+    for t = 1:T
+        # simulate
+        xsim[t+1], ξsim[t] = propose(xsim[t], gradlπ, sampler)
+    end
+    return xsim[1:T], ξsim
 end
 
 
 function sample(gradlπ::Function, sampler::ULA, T::Int64, x0::Vector{<:Real})
     d = length(x0)
-    xsim = [zeros(d) for t = 1:T]; xsim[1] = x0
+    xsim = [zeros(d) for t = 1:T+1]; xsim[1] = x0
     ξsim = [zeros(d) for t = 1:T]
-    for t = 2:T
-        # simulate
-        xsim[t], ξsim[t] = propose(xsim[t-1], gradlπ, sampler)
+
+    if sampler.seed != nothing
+        Random.seed!(sampler.seed)
     end
-    return xsim, ξsim
+    
+    for t = 1:T
+        # simulate
+        xsim[t+1], ξsim[t] = propose(xsim[t], gradlπ, sampler)
+    end
+    return xsim[1:T], ξsim
 end
 
 sample(lπ::Function, gradlπ::Function, sampler::ULA, T::Int64, x0::Union{Real, Vector{<:Real}}) = sample(gradlπ, sampler, T, x0)
