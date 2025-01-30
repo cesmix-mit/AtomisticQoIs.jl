b38011408fb34428b86cf8b7f70a86902c5164b6
joannajzou <joannajzou@gmail.com>
2025-01-30 11:02:26 -0500

    update MCMC to subsample from paths of length T

--- src/Integrators/monte_carlo.jl
+++ src/Integrators/monte_carlo.jl
@@ -21,15 +21,18 @@ Contains parameters for Monte Carlo (MC) integration using MCMC samples.
 This method is implemented when the distribution cannot be analytically sampled.
 
 # Arguments
+- `T :: Int`               : number of simulation steps
 - `n :: Int`               : number of samples
 - `sampler :: Sampler`     : type of sampler (see `Sampler`)
 - `ρ0 :: Union{Distribution, Real, Vector{<:Real}}`     : initial state or prior distribution of the state
 """
 struct MCMC <: MCIntegrator
+    T :: Int 
     n :: Int
     sampler :: Sampler
     ρ0 :: Union{Distribution, Real, Vector{<:Real}}
 end
+MCMC(n::Int, sampler::Sampler, ρ0::Union{Distribution, Real, Vector{<:Real}}) = MCMC(n, n, sampler, ρ0)
 
 
 """
